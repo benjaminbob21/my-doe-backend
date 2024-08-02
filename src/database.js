1dccfd29e5baf20ebde676f534a14f70ecf44a71
Benjamin Bamisile <bambobtim@gmail.com>
2024-08-01 22:04:42 -0500

    little change

--- src/database.js
+++ src/database.js
@@ -3,9 +3,19 @@ import { Sequelize } from 'sequelize';
 export const sequelize = new Sequelize("doe-project", "postgres", " ", {
   host: "localhost",
   dialect: "postgres",
-  port: process.env.PORT
+  port: process.env.DB_PORT
 });
 
+// export const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//     port: process.env.DB_PORT,
+//   }
+// );
 
 sequelize
   .authenticate()
